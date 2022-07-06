ad2a2be8622f2887a80107704cc86952311f0047
Jorge Quintero <rouchjq8@gmail.com>
2022-07-06 17:41:59 -0400

    ultimos ajustes a FAQS

--- pages/faqs/index.tsx
+++ pages/faqs/index.tsx
@@ -17,70 +17,94 @@ const Faqs: NextPage = () => {
               }}
             >
               <Collapse title="Who are we">
-                <Text size={18}>
-                  We are Looties and we believe in you having a fair game. We
-                  are a Web 3 smart contract built on the Solana blockchain that
-                  allows users to open Lootboxes in Sol and win Sol in return.
-                  There is a 4% transaction fee for every Lootbox opened which
-                  is split up into:
-                </Text>
-                <ul>
-                  <li>
-                    <Text size={18}>⚪ 3% to Looties NFT holders</Text>
-                  </li>
-                  <li>
-                    <Text size={18}>⚪ 0.5% to the house wallet</Text>
-                  </li>
-                  <li>
-                    <Text size={18}>⚪ 0.5% to the team wallet </Text>
-                  </li>
-                </ul>
+                <Container>
+                  <Text size={18}>
+                    We are Looties and we believe in you having a fair game. We
+                    are a Web 3 smart contract built on the Solana blockchain
+                    that allows users to open Lootboxes in Sol and win Sol in
+                    return. There is a 4% transaction fee for every Lootbox
+                    opened which is split up into:
+                  </Text>
+                  <ul>
+                    <li>
+                      <Text size={18}>⚪ 3% to Looties NFT holders</Text>
+                    </li>
+                    <li>
+                      <Text size={18}>⚪ 0.5% to the house wallet</Text>
+                    </li>
+                    <li>
+                      <Text size={18}>⚪ 0.5% to the team wallet </Text>
+                    </li>
+                  </ul>
+                </Container>
               </Collapse>
               <Collapse title="what are the odds">
-                <Text size={18}>
-                  All our games have a 100% Return To Player (RTP) there is no
-                  house edge or margin.
-                </Text>
+                <Container>
+                  <Text size={18}>
+                    All our games have a 100% Return To Player (RTP) there is no
+                    house edge or margin.
+                  </Text>
+                </Container>
               </Collapse>
               <Collapse title="what are the prizes">
-                <Text size={18}>
-                  0X, 0.5X, 1X, 2X, 5X, 10X, 25X, 50X, 100X.
-                </Text>
+                <Container>
+                  <Text size={18}>
+                    0X, 0.5X, 1X, 2X, 5X, 10X, 25X, 50X, 100X.
+                  </Text>
+                </Container>
               </Collapse>
               <Collapse title="what are the prizes">
-                <Text size={18}>
-                  Transactions on the smart contract are verified on chain.
-                </Text>
-                <ol>
-                  <li>
-                    <Text size={18}>
-                      House wallet :
-                      55TLUPqASKcVLAbYcio7FgyioUmtiGD68RQRDFgsztH8
-                    </Text>
-                  </li>
-                  <li>
-                    <Text size={18}>
-                      Fee wallet : 7RkSs75YG6kVg8PEHoSUFGukZFBqe44onubzBAtRX9pZ
-                    </Text>
-                  </li>
-                </ol>
+                <Container>
+                  <Text size={18}>
+                    Transactions on the smart contract are verified on chain.
+                  </Text>
+                  <ol>
+                    <li>
+                      <Text size={18}>
+                        House wallet :
+                        55TLUPqASKcVLAbYcio7FgyioUmtiGD68RQRDFgsztH8
+                      </Text>
+                    </li>
+                    <li>
+                      <Text size={18}>
+                        Fee wallet :
+                        7RkSs75YG6kVg8PEHoSUFGukZFBqe44onubzBAtRX9pZ
+                      </Text>
+                    </li>
+                  </ol>
+                </Container>
               </Collapse>
               <Collapse title="what are the prizes">
-                <Container display="flex" justify="flex-start" fluid>
+                <Container
+                  display="flex"
+                  justify="flex-start"
+                  fluid
+                  css={{
+                    '@media (max-width: 768px)': {
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    },
+                  }}
+                >
+                  <Text
+                    size={18}
+                    css={{
+                      margin: '0 1rem 0 0',
+                      '@media (max-width: 768px)': {
+                        margin: '0',
+                      },
+                    }}
+                  >
+                    We will never DM you first. Only use the official channels.
+                  </Text>
                   <TwitterIcon
                     sx={{
                       color: 'white',
                       fontSize: '2rem',
                     }}
                   />
-                  <Text
-                    size={18}
-                    css={{
-                      margin: '0 1rem',
-                    }}
-                  >
-                    We will never DM you first. Only use the official channels.
-                  </Text>
                 </Container>
               </Collapse>
             </Collapse.Group>
